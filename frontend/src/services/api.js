ea658344bcf30fda625ce385419dfe385f82503d
tanmay <tanmaylagoo@gmail.com>
2026-08-29 18:53:36 +0530

    Connect frontend to backend deployed on render

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,8 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    import.meta.env.VITE_API_URL ||
-    "http://127.0.0.1:8000",
+ import.meta.env.VITE_API_URL,
 
   headers: {
     "Content-Type": "application/json",
